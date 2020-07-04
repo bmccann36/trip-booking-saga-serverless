fc9729728ce634761ee0ecc002ce34c3260758d3
mccann-b <brian@cedrus.digital>
2020-07-04 14:41:52 -0400

    accept region as arg to script

--- zeebe-event-adapter/aws/setupEcs.sh
+++ zeebe-event-adapter/aws/setupEcs.sh
@@ -1,6 +1,12 @@
 #! /bin/bash
+# check arguments
+if [[ "$#" -lt 1 ]]; then
+  echo "please supply a region as single argument"
+  echo "Usage: $0 <your region> " >&2
+  exit 1
+fi
 ## cluster config
-ecs-cli configure --cluster zeebe-lambda-worker --default-launch-type EC2 --config-name zb-cfg --region us-east-1
+ecs-cli configure --cluster zeebe-lambda-worker --default-launch-type EC2 --config-name zb-cfg --region "$1"
 
 ## create cluster
 ecs-cli up --capability-iam --size 1 --instance-type t2.micro --cluster-config zb-cfg
