479a260cdd926eaae3daf5ccfeb7932f35a7def9
chylomicronman <bmccann36@gmail.com>
2020-07-12 15:25:10 -0400

    got rid of nested folder so ECS task has more normal name

--- zeebe-event-adapter/setupEcs.sh
+++ zeebe-event-adapter/setupEcs.sh
@@ -12,7 +12,7 @@ ecs-cli configure --cluster zeebe-lambda-worker --default-launch-type EC2 --conf
 ecs-cli up --capability-iam --size 1 --instance-type t2.micro --cluster-config zb-cfg
 
 ## set it up as a service
-ecs-cli compose service up --cluster-config zb-cfg --create-log-groups 
+ecs-cli compose service up --cluster-config zb-cfg  --create-log-groups 
 
 ## scale the service
 ecs-cli compose service scale 2 --cluster-config zb-cfg
